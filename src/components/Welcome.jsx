d7ff2884ced29c760e755c9589841a52ef6c4c87
xingyin2024 <xingyin.thu@gmail.com>
2024-10-15 17:07:10 +0200

    update Welcome

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -8,7 +8,7 @@ export const Welcome = () => {
     <>
       <h1>The Science of Happiness</h1>
       <h2>Everday Joy Bossters Survey</h2>
-      <button className="button">Let's go!</button>
+      <button className="button">Let us go!</button>
     </>
   )  
 }
